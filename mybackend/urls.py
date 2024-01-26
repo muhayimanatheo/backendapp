bc80063e345c1a2c487f8076eb337ccbe9e7561c
muhayimanatheo <muhayitheo23@gmail.com>
2024-01-26 14:07:05 +0200

    serializer post successfull

--- mybackend/urls.py
+++ mybackend/urls.py
@@ -3,5 +3,5 @@ from. import views
 
 urlpatterns = [
     path('UserRegisterApi/', views.UserRegisterApi),
-    path('', views.index , name='index'),
+    # path('', views.index , name='index'),
 ]
--- mybackend/views.py
+++ mybackend/views.py
@@ -2,15 +2,26 @@ from django.shortcuts import render
 from .models import *
 from .serializers import*
 from django .http import JsonResponse
+from rest_framework.parsers import JSONParser
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 def index(request):
     return render(request, "index.html")
 
-
+@csrf_exempt
 def UserRegisterApi(request):
     if request.method == 'GET':
         selectedData = UserRegister.objects.all()
         serializer = UserRegisterSerializer(selectedData, many=True)
-    
         return JsonResponse(data=serializer.data, safe=False)
+    
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = UserRegisterSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+        
